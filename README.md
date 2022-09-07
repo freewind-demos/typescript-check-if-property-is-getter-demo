e01f42c2104251f677deb27c26a967b77af79279
Freewind <nowindlee@gmail.com>
2022-09-07 17:47:21 +0800

    typescript-check-if-property-is-getter-demo

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
-TypeScript Hello World Demo
+TypeScript Check If Property is Getter Demo
 ===========================
+
+检查对象中的某个property是否为getter。
+
+有些对象的getter实际上做了一些有副作用的事情，我们有时候需要把它们过滤出去
 
 ```
 npm install
-npm run demo
-```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
+npm start
 ```
 
--- hello.ts
+++ hello.ts
@@ -1,1 +1,21 @@
-console.log('Hello');
+const user = {
+    name: 'typescript',
+    get upperName() {
+        console.log("called this getter!!!")
+        return this.name.toUpperCase();
+    },
+}
+
+function main() {
+    for (const propertyName of Object.keys(user)) {
+        console.log('### ', propertyName);
+        const descriptor = Object.getOwnPropertyDescriptor(user, propertyName)
+        console.log('### descriptor', descriptor);
+
+        if (descriptor !== undefined && typeof descriptor.get === 'function') {
+            console.log(`### "${propertyName}" is a getter!`)
+        }
+    }
+}
+
+main();
